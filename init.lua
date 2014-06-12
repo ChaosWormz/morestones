c6f7c989e459d1044f3abe26db38a757346ccb76
Craig Robbins <crobbins@localhost.localdomain>
2014-06-12 16:27:49 +1000

    Reformatted code and fixed minor spelling mistake

--- init.lua
+++ init.lua
@@ -1,89 +1,89 @@
 --register node
 minetest.register_node("morestones:limestone", {
-description = "Lime stone",
-tiles = {"stones_plus_limestone.png"},
-is_ground_content = true,
-groups = {cracky=2, stone=1},
-legacy_mineral = true,
-sounds = default.node_sound_stone_defaults(),
+    description = "Limestone",
+    tiles = {"stones_plus_limestone.png"},
+    is_ground_content = true,
+    groups = {cracky=2, stone=1},
+    legacy_mineral = true,
+    sounds = default.node_sound_stone_defaults(),
 })
 
 
 minetest.register_node("morestones:marble", {
-description = "Marble",
-tiles = {"stones_plus_marble.png"},
-is_ground_content = true,
-groups = {cracky=2, stone=1},
-legacy_mineral = true,
-sounds = default.node_sound_stone_defaults(),
+    description = "Marble",
+    tiles = {"stones_plus_marble.png"},
+    is_ground_content = true,
+    groups = {cracky=2, stone=1},
+    legacy_mineral = true,
+    sounds = default.node_sound_stone_defaults(),
 })
 
 minetest.register_node("morestones:slate", {
-description = "Slate",
-tiles = {"stones_plus_slate.png"},
-is_ground_content = true,
-groups = {cracky=2, stone=1},
-legacy_mineral = true,
-sounds = default.node_sound_stone_defaults(),
+    description = "Slate",
+    tiles = {"stones_plus_slate.png"},
+    is_ground_content = true,
+    groups = {cracky=2, stone=1},
+    legacy_mineral = true,
+    sounds = default.node_sound_stone_defaults(),
 })
 
 minetest.register_node("morestones:travertine", {
-description = "Travertine",
-tiles = {"stones_plus_travertine.png"},
-is_ground_content = true,
-groups = {cracky=2, stone=1},
-legacy_mineral = true,
-sounds = default.node_sound_stone_defaults(),
+    description = "Travertine",
+    tiles = {"stones_plus_travertine.png"},
+    is_ground_content = true,
+    groups = {cracky=2, stone=1},
+    legacy_mineral = true,
+    sounds = default.node_sound_stone_defaults(),
 })
 
---register ore
+--register ores for mapgen
 
 minetest.register_ore({
-ore_type = "scatter",
-ore = "morestones:limestone",
-wherein = "default:stone",
-clust_scarcity = 15*15*15,
-clust_num_ores = 12,
-clust_size = 4,
-height_min = -31000,
-height_max = -1000,
-flags = "absheight",
-})
-
-minetest.register_ore({
-ore_type = "scatter",
-ore = "morestones:marble",
-wherein = "default:stone",
-clust_scarcity = 21*21*21,
-clust_num_ores = 12,
-clust_size = 7,
-height_min = -31000,
-height_max = -1500,
-flags = "absheight",
+    ore_type = "scatter",
+    ore = "morestones:limestone",
+    wherein = "default:stone",
+    clust_scarcity = 15*15*15,
+    clust_num_ores = 12,
+    clust_size = 4,
+    height_min = -31000,
+    height_max = -1000,
+    flags = "absheight",
 })
 
 minetest.register_ore({
-ore_type = "scatter",
-ore = "morestones:slate",
-wherein = "default:stone",
-clust_scarcity = 21*21*21,
-clust_num_ores = 16,
-clust_size = 12,
-height_min = -31000,
-height_max = -2100,
-flags = "absheight",
+    ore_type = "scatter",
+    ore = "morestones:marble",
+    wherein = "default:stone",
+    clust_scarcity = 21*21*21,
+    clust_num_ores = 12,
+    clust_size = 7,
+    height_min = -31000,
+    height_max = -1500,
+    flags = "absheight",
 })
 
 minetest.register_ore({
-ore_type = "scatter",
-ore = "travertine",
-wherein = "default:stone",
-clust_scarcity = 25*25*25,
-clust_num_ores = 18,
-clust_size = 14,
-height_min = -31000,
-height_max = -1500,
-flags = "absheight",
+    ore_type = "scatter",
+    ore = "morestones:slate",
+    wherein = "default:stone",
+    clust_scarcity = 21*21*21,
+    clust_num_ores = 16,
+    clust_size = 12,
+    height_min = -31000,
+    height_max = -2100,
+    flags = "absheight",
+})
+
+minetest.register_ore({
+    ore_type = "scatter",
+    ore = "travertine",
+    wherein = "default:stone",
+    clust_scarcity = 25*25*25,
+    clust_num_ores = 18,
+    clust_size = 14,
+    height_min = -31000,
+    height_max = -1500,
+    flags = "absheight",
 })
 
 dofile(minetest.get_modpath("morestones") .. "/stairs.lua")
--- stairs.lua
+++ stairs.lua
@@ -1,27 +1,32 @@
 -- lime Stone
-stairs.register_stair_and_slab("limestone", "morestones:limestone",
+stairs.register_stair_and_slab(
+    "limestone", "morestones:limestone",
 	{cracky=3},
 	{"stones_plus_limestone.png"},
-	"Lime Stone stair",
-	"Lime Stone slab")
+	"Limestone stair",
+	"Limestone slab")
 
 -- Marble Stone
-stairs.register_stair_and_slab("marble", "morestones:marble",
+stairs.register_stair_and_slab(
+    "marble", "morestones:marble",
 	{cracky=3},
 	{"stones_plus_marble.png"},
 	"Marble stair",
 	"Marble slab")
 
 -- Slate Stone
-stairs.register_stair_and_slab("slate", "morestones:slate",
+stairs.register_stair_and_slab(
+    "slate", "morestones:slate",
 	{cracky=3},
 	{"stones_plus_slate.png"},
 	"Slate stair",
 	"Slate slab")
 
 -- travertine Stone
-stairs.register_stair_and_slab("travertine", "morestones:travertine",
+stairs.register_stair_and_slab(
+    "travertine", "morestones:travertine",
 	{cracky=3},
 	{"stones_plus_travertine.png"},
 	"Travertine stair",
-	"Travertine slab")
+	"Travertine slab"
+)
